b620a79eb7dc173449f92d99d5c03c830b7c4455
externalPointerVariable <vicky986847@gmail.com>
2025-02-10 15:35:24 +0000

    Added unlimited colors project in the projects directory ✅

--- js/projects/unlimitedColors.js
+++ js/projects/unlimitedColors.js
@@ -1,5 +1,41 @@
 export const unlimitedColors = {
     title: 'Unlimited Colors🌈',
-    code:``,
-    html:``,
+    code:`
+        const unlimitedColorsContainer = document.querySelector('.unlimitedcolors');
+        const unlimitedColors = function() {
+            const hexVal = '123456789ABCDEF';
+            let color = '#';
+            for (let i = 0; i < 6; i++) {
+                color += hexVal[Math.floor(Math.random() * 16)];
+            }
+            return color;
+        };
+
+        let intervalId;
+
+        const startUnlimitedColors = function() {
+            if(!intervalId) {
+                intervalId = setInterval(() => {
+                    unlimitedColorsContainer.style.backgroundColor = unlimitedColors();
+                }, 100);
+            }
+        };
+
+        const stopUnlimitedColors = function() {
+            clearInterval(intervalId);
+            intervalId = null;
+        };
+
+        document.getElementById('colorStart').addEventListener('click', startUnlimitedColors);
+        document.getElementById('colorStop').addEventListener('click', stopUnlimitedColors);
+    `,
+    html:`
+    <div class="unlimitedcolors rounded">
+        <div class="alert pb-5">
+            <p class="lead text-black">Unlimited Colors</p>
+            <button class="btn btn-primary" id="colorStart">Start</button>
+            <button class="btn btn-danger" id="colorStop">Stop</button>
+        </div>
+    </div>
+    `,
 }
