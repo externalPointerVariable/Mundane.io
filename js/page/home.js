c75dc61fff4e210add5df235abd2439a9bc55bcb
externalPointerVariable <vicky986847@gmail.com>
2025-02-10 16:24:40 +0000

    Made some changes in home and todo list project

--- js/page/home.js
+++ js/page/home.js
@@ -10,7 +10,7 @@ export async function loadHome() {
     main.innerHTML = `
         <section class="text-center translate-x-6">
             <h1 class="display-1 mb-3 p-5 pb-2 text-warning font-monospace">Mundane.io</h1>
-            <p class="blockquote text-white">A collection of basic JavaScript projects to help you learn and experiment</p>
+            <p class="blockquote text-white">A collection of basic <kbd class="text-info">JavaScript</kbd> projects to help you learn and experiment</p>
             <a href="/projects" class="btn btn-warning btn-lg mt-3" data-link>Explore Projects</a>
         </section>
     `;
